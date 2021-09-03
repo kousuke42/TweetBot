4dff8b74d67d7d0b2f624e0f654a57898a627c5d
kousuke42 <alchemy.rad@gmail.com>
2021-09-04 00:53:16 +0900

    Initial commitment.

--- index.py
+++ index.py
@@ -1,10 +1,7 @@
-#coding:utf-8
-import os
-from bottle import route, run
+from bottle import route, run, template
 
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
 
-@route("/")
-def hello_world():
-        return "hello world"
-
-run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+run(host='localhost', port=8080)
